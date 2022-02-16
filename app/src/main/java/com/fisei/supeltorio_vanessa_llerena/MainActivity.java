90acb4491bc6415bb3c46be6243edf8dd5c4c6fd
Vanessa Llerena <vanestefania01@gmail.com>
2022-02-16 18:04:48 -0500

    Initial commit

---
+++ app/src/main/java/com/fisei/supeltorio_vanessa_llerena/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fisei.supeltorio_vanessa_llerena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
